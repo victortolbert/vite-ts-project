cd3bf4b80b494447bf2559cb0b4d199b78ae82e6
Victor <victor.tolbert@gmail.com>
2022-07-18 19:44:55 -0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    <h1>{{ msg }}</h1>
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
